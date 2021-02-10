5401649e64a2668b1859995c7d25abe142982f90
Bryanoxx <contact@bryanparasote.fr>
2021-02-10 14:41:20 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components: { HelloWorld }
+}
+</script>
+
+<style scoped>
+.main {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <p>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
+    </p>
+
+    <button @click="count++">count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      count: 0
+    }
+  },
+  props: {
+    msg: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
